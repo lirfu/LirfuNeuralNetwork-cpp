8db9c1f7654905492be2b7f601b4d9cd7ff463dd
lirfu <jurajfulir@gmail.com>
2018-04-25 23:52:56 +0200

    Update demos to use the NetworkBuilder and Grapher

--- demos/Demos.cpp
+++ demos/Demos.cpp
@@ -15,6 +15,7 @@
 #include "../functions/Sigmoid.h"
 #include "../functions/Linear.h"
 #include "../data/SimpleData.h"
+#include "../NetworkBuilder.h"
 
 void Demos::demoRegression() {
     try {
@@ -55,31 +56,38 @@ void Demos::demoRegression() {
 //        layers[1]->setNeuron(0, weights);
 //        layers[1]->setNeuron(1, weights);
 
-        cout << "init 4" << endl; // Initialize graph with 2 curves
-
+#ifdef OUTPUT_RESULTS
+        cout << "init 2" << endl; // Initialize graph with 2 curves
+#endif
         // Run backpropagation.
         uint iteration = 0;
         double error = 1000;
-        while (error > 1e-4) {
+        while (error > 1e-1) {
             error = network.backpropagate(1e-3, batches);
             iteration++;
 
             // Report progress.
             if (iteration % 1000 == 0) {
-                std::cout << "echo Iteration " << iteration << " has error: "
+#ifdef OUTPUT_RESULTS
+                std::cout << "echo";
+#endif
+                std::cout << "Iteration " << iteration << " has error: "
                           << setprecision(16) << error << std::endl;
-
+#ifdef OUTPUT_RESULTS
                 cout << "clear" << endl; // Clear the graphs.
                 Matrix out;
                 for (uint i = 0; i < numberOfPoints; i++) {
                     out = network.getOutput(*inputs.at(i));
                     // Add the target and predicted output values.
-                    std::cout << "add " << out.get(0, 0) << " " << out.get(0, 1) << " " <<
-                              outputs.at(i)->get(0, 0) << " " << outputs.at(i)->get(0, 1) << std::endl;
+                    std::cout << "add " << out.get(0, 0) << " " << outputs.at(i)->get(0, 0) << std::endl;
                 }
+#endif
             }
         }
-        std::cout << "echo Finally, iteration " << iteration << " has error: "
+#ifdef OUTPUT_RESULTS
+        std::cout << "echo";
+#endif
+        std::cout << "Finally, iteration " << iteration << " has error: "
                   << setprecision(16) << error << std::endl;
 
         // Cleanup data.
@@ -89,4 +97,94 @@ void Demos::demoRegression() {
     } catch (string s) {
         std::cout << s << endl;
     }
+}
+
+void ::Demos::demoGPBuilder() {
+    try {
+        uint numberOfPoints = 200;
+
+        // Create the data.
+        std::vector<Matrix *> inputs;
+        std::vector<Matrix *> outputs;
+        for (int i = 0; i < numberOfPoints; i++) {
+            double input = i * 2. * M_PI / numberOfPoints;
+            inputs.push_back(new Matrix(1, 1, {input}));
+            outputs.push_back(new Matrix(1, 1, {0.2 * sin(input) + 0.2 * sin(4 * input + M_PI / 7) + 0.5}));
+        }
+        std::vector<Data *> batches;
+        batches.push_back(new SimpleData(&inputs, &outputs));
+
+        // Build the network.
+        WeightInitializer* initializer = new RandomWeightInitializer(-1,1);
+        InputLayer *inputLayer = new InputLayer(1);
+        NeuralNetwork network = NetworkBuilder(inputLayer)
+                .addLayer(20)
+                .addLayer(16)
+                .addLayer(7)
+                .addLayer(1, NetworkBuilder::FULLY_CONNECTED, NetworkBuilder::LINEAR)
+                .build(initializer);
+        delete initializer;
+
+#ifdef OUTPUT_RESULTS
+        cout << "init 2" << endl; // Initialize graph with 2 curves
+#endif
+        // Run backpropagation.
+        uint iteration = 0;
+        double error = 1000;
+        long iterator = 100000L;
+        while (iterator-- > 0 /*error > 1e-3*/) {
+            error = network.backpropagate(1, batches);
+            iteration++;
+
+            // Report progress.
+            if (iteration % 1000 == 0) {
+#ifdef OUTPUT_RESULTS
+                std::cout << "echo ";
+#endif
+                std::cout << "Iteration " << iteration << " has error: "
+                          << setprecision(16) << error << std::endl;
+#ifdef OUTPUT_RESULTS
+                cout << "clear" << endl; // Clear the graphs.
+                Matrix out;
+                for (uint i = 0; i < numberOfPoints; i++) {
+                    out = network.getOutput(*inputs.at(i));
+                    // Add the target and predicted output values.
+                    std::cout << "add " << out.get(0, 0) << " " << out.get(0, 1) << " " <<
+                              outputs.at(i)->get(0, 0) << " " << outputs.at(i)->get(0, 1) << std::endl;
+                }
+#endif
+            }
+        }
+#ifdef OUTPUT_RESULTS
+        std::cout << "echo ";
+#endif
+        std::cout << "Finally, iteration " << iteration << " has error: "
+                  << setprecision(16) << error << std::endl;
+
+        // Cleanup data.
+        for (Data *sd : batches) {
+            delete sd;
+        }
+    } catch (string s) {
+        std::cout << s << endl;
+    }
+}
+
+void ::Demos::demoXORProblem() {
+    std::vector<Matrix *> inputs;
+    inputs.push_back(new Matrix(1, 2, {0, 0}));
+    inputs.push_back(new Matrix(1, 2, {0, 1}));
+    inputs.push_back(new Matrix(1, 2, {1, 0}));
+    inputs.push_back(new Matrix(1, 2, {1, 1}));
+    std::vector<Matrix *> outputs;
+    outputs.push_back(new Matrix(1, 1, {0}));
+    outputs.push_back(new Matrix(1, 1, {1}));
+    outputs.push_back(new Matrix(1, 1, {1}));
+    outputs.push_back(new Matrix(1, 1, {0}));
+
+
+    std::vector<Data *> batches;
+    batches.push_back(new SimpleData(&inputs, &outputs));
+
+    // TODO Model definition...
 }
